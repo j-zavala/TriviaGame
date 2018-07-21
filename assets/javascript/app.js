40f8cdd7d7384e06cdd71e86f1ffb60a04526527
Johnny Zavala <jzavalafgz@gmail.com>
2018-07-21 07:40:29 -0400

    Cleaned up the styling and added the choices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,206 @@
-let stopwatch = {
-    time: 31,
+let onStart = {
 
-    reset: function()
-    {
-        stopwatch.time = 20;
-
-        // DONE: Change the "display" div to "00:00."
-        $(".display").text("20:00");
-    },
-
-    start: function() {
-        // Use setInterval to start the count here and set the clock to running.
-        if (!clockRunning)
+    combos = [
         {
-            console.log("In start");
-            intervalId = setInterval(stopwatch.count, 1000);
-            clockRunning = true;
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: false
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
+        },
+        {
+            question: "How old is Johnny?",
+            choices: [
+                {
+                    choice: "Just born",
+                    value: false
+                },
+                {
+                    choice: "23",
+                    value: true
+                },
+                {
+                    choice: "16",
+                    value: true
+                },
+                {
+                    choice: "46",
+                    value: false
+                }
+            ]
         }
+    ],
+
+    loadAnswers: function() {
+        //select each question and store in a variable
+
+        //create 
+
+        //
     },
 
-    stop: function() {
-        console.log("In stop");
-        // Use clearInterval to stop the count here and set the clock to not be running.
-        clearInterval(intervalId);
-        clockRunning = false;
-
-        // Change the "display" div to "00:00."
-        // $("#display").text(stopwatch.count());
-        
+    startTimer: function() {
 
     },
 
-    count: function() 
-    {
-        // increment time by 1, remember we cant use "this" here.
+    respondToClicks: function() {
+        $("input[type='radio']").click(function(){
 
-        if (stopwatch.time <= 0) {
-            stopwatch.stop();
-        }
-        stopwatch.time--;
+            var radioValue = $("input[name='age']:checked").val();
 
-        // Get the current time, pass that into the stopwatch.timeConverter function,
-        //       and save the result in a variable.
-        var converted = stopwatch.timeConverter(stopwatch.time);
-        console.log(converted);
+            if(radioValue){
 
-        // Use the variable we just created to show the converted time in the "display" div.
-        $(".display").text(converted);
-    },
+                alert("Your age is - " + radioValue);
 
-    timeConverter: function(t) {
+            }
 
-        var minutes = Math.floor(t / 60);
-        var seconds = t - (minutes * 60);
-    
-        if (seconds < 10) {
-          seconds = "0" + seconds;
-        }
-    
-        if (minutes === 0) {
-          minutes = "00";
-        }
-        else if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
-    
-        return minutes + ":" + seconds;
-      }
+        });
+    }
 }
+
+
+
+$( document ).ready(function() {
+    onStart.respondToClicks();
+    
+});
